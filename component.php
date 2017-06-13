2d61026996fa5e2867016bade6517aa5adcfc566
Patrick Smits <familie.smits@caiway.nl>
2017-06-13 21:08:59 +0200

    Update component.php

--- component.php
+++ component.php
@@ -1,5 +1,5 @@
 <?php 
-	/*------------------------------------------------------------------------
+/*------------------------------------------------------------------------
 # author    Patrick Smits
 # copyright © 2017 byAntoinette Communicatie All rights reserved.
 # @license  http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
